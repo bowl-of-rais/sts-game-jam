5ea6b0df38293d5749e901b710a173213dd8de75
anieway <anieway@tuta.io>
2025-01-25 14:17:04 +0100

    finished base dialog system

--- this-place-saves-lives/Scripts/dialog_player.gd
+++ this-place-saves-lives/Scripts/dialog_player.gd
@@ -1,11 +1,17 @@
 extends DramaDisplayControl
 
 @onready var dialog_text_field = get_node("DialogPanel/DialogTextLabel")
+@onready var dialog_box = get_node("DialogPanel")
 @onready var answer_button1 = get_node("AnswerButton1")
 @onready var answer_button2 = get_node("AnswerButton2")
-@onready var player_text_field = get_node("PlayerTextLabel")
-
+@onready var player_text_field = get_node("PlayerPanel/PlayerTextLabel")
+@onready var player_box = get_node("PlayerPanel")
 @onready var speaker_text_field = dialog_text_field
+
+var dialog_scene = null
+
+var dialog_ready = false
+var dialog_running = false
 
 func _ready() -> void:
 	#connect to signal that indicates character waiting at desk
@@ -14,19 +20,28 @@ func _ready() -> void:
 	
 
 func _on_dialog_waiting(dialog_name):
+	assert(not dialog_ready, "no dialog should be waiting before getting another ready")
 	#load dialog scene instance
 	var path: String = "res://Events/" + dialog_name + ".tscn"
 	add_child(await load(path).instantiate())
-	#load dialog text
-	#json ressource in dialog scene or dialog tree
+	dialog_scene = get_node("Dialog")
+	#load gdrama dialog
+	%DramaPlayer.load_gdrama("res://Dialog/" + dialog_name + ".gdrama")
+	self.dialog_ready = true
+	self.visible = true
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton \
-	 and event.button_index == 0 \
+	 and event.button_index == 1 \
 	 and event.pressed :
 		#if the dialog player was left clicked:
 		#advance dialog if more lines are available and no decision is pending
-		self.drama_player.next_or_skip()
+		if dialog_running:
+			self.drama_player.next_or_skip()
+		elif dialog_ready:
+			self.dialog_ready = false
+			self.dialog_running = true
+			%DramaPlayer.start_drama()
 		
 func hide_and_disable_buttons():
 	answer_button1.disabled = true
@@ -39,12 +54,13 @@ func hide_and_disable_buttons():
 ## Will be called when the dialogue has been skipped. It can be used, for
 ## instance, for drawing all letters of the dialogue
 func _skipped():
-	pass
+	speaker_text_field.visible_characters = len(speaker_text_field.text)
 
 ## Will be called when the current direction ends. It can be used, for instance,
 ## to add a visual indicator that the dialogue can be skipped
 func _direction_ended():
 	pass
+	#TODO add indicator?
 
 ## Will be called to signal that the given text should be displayed in the main
 ## label. Can, for instance, set this text to a RichTextLabel and update its
@@ -97,16 +113,27 @@ func _ask_for_choice(line: Dictionary):
 ## as _set_raw_text and _spoke, so it may be used to determine which display
 ## to focus on out of many
 func _set_actor(actor: String):
-	pass
 	if actor.to_lower() == "player":
 		speaker_text_field = player_text_field
-		player_text_field.visible = true
+		player_box.visible = true
+		dialog_box.visible = false
 	else:
 		speaker_text_field = dialog_text_field
+		dialog_box.visible = true
+		player_box.visible = false
 
 ## Will be called to signal that the current scene has ended. Can be used, for
 ## instance, to close the current dialogue windows and free the player's
 ## movement
-func _ended_drama(info: String):
-	pass
-	#TODO remove event scene (containing character sprite and mb other stuff)
+func _ended_drama(_info: String):
+	self.dialog_running = false
+	#remove event scene (containing character sprite and mb other stuff)
+	self.visible = false
+	#TODO do some fading out
+	get_node("Dialog").queue_free()
+	
+## Will be emited when the GDrama calls for a function. Can be used to implement
+## specific functions, such as changing a character's mood
+func _drama_call(method_name: String, args: Array):
+	if dialog_scene.has_method(method_name):
+		dialog_scene.callv(method_name, args)
--- this-place-saves-lives/Scripts/main.gd
+++ this-place-saves-lives/Scripts/main.gd
@@ -1,11 +1,15 @@
 extends Node
 
+var emitted = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not emitted:
+		SignalBus.dialog_waiting.emit("marel1")
+		emitted = true
