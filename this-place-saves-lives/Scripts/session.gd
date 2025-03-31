daad9f1916d7a628fcb6aadba69109a886cd8bcb
Rai T <mira@trouvain.com>
2025-04-01 00:36:24 +0200

    added vars for story flags/character states in session

--- this-place-saves-lives/Scripts/session.gd
+++ this-place-saves-lives/Scripts/session.gd
@@ -6,8 +6,6 @@ var funds: int = 1000:
 	get():
 		return funds
 
-# TODO: connect to signals?
-
 func change_funds(val: int):
 	# TODO: handle case where val < 0 && |val| > funds
 	funds += val
@@ -16,20 +14,41 @@ var approval: int = 50:
 	get():
 		return approval
 
-# TODO: connect to signals?
-
 func change_approval(val: int):
 	# TODO: handle case where val < 0 && |val| > funds
 	approval += val
 
 # ------------------------------- SERVICES -------------------------------------
 
+var max_per_service: Dictionary[Services.Types, int]
 
+var unlocked_stations: Dictionary[Services.Types, int]
+
+# ------------------------------ CHARACTERS ------------------------------------
+
+var characters: Dictionary[String, CharacterSetting]
+
+var true_story_flags: Array[String]
+
+# adds a flag to the set of globally tracked story flags
+func add_story_flag(name: String):
+	true_story_flags.append(name)
+
+# for an array of story flags, checks if they are all true
+func check_story_flags(flags: Array[String]) -> bool:
+	for f in flags:
+		if f not in true_story_flags:
+			return false
+	return true
 
 # -------------------------------- SAVING --------------------------------------
 
 func save_game():
 	pass
 	
-func load_game():
+func load_save_game():
+	pass
+
+func initialize_game():
+	# read in files: initial game state, initial character states
 	pass
