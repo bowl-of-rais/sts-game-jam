694c7cbd886ebd3317e0d3f02a06d22264178f42
Rai T <mira@trouvain.com>
2025-04-01 13:47:41 +0200

    added loading of character settings

--- this-place-saves-lives/GameState/game_state_manager.gd
+++ this-place-saves-lives/GameState/game_state_manager.gd
@@ -10,6 +10,7 @@ func load_saved_game():
 func initialize_game():
 	# read in files: initial game state, initial character states
 	load_game_state("res://GameState/init_session.cfg")
+	load_character_states("res://GameState/init_characters.cfg")
 	
 func start_game():
 	get_tree().change_scene_to_file("res://Views/Main.tscn")
@@ -49,3 +50,30 @@ func load_game_state(path: String):
 		var service = Services.service_from_name(key)
 		if service != -1:
 			Session.unlocked_per_service[service] = read_dict[key]
+
+func load_character_states(path: String):
+	var config = ConfigFile.new()
+
+	# Load data from a file.
+	var err = config.load(path)
+
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+
+	for character in config.get_sections():
+		var char_res: CharacterSetting = CharacterSetting.new()
+		char_res.name = config.get_value(character, "name")
+		char_res.bodytype = CharacterSetting.bodytype_from_str(config.get_value(character, "bodytype"))
+		char_res.overdose_risk = config.get_value(character, "overdose_risk")
+		char_res.initial_needs = []
+		
+		var needs_str = config.get_value(character, "overdose_risk")
+		for s in needs_str:
+			var need = CharacterSetting.need_from_str(s)
+			if need != -1:
+				char_res.initial_needs.append(need)
+				
+		# TODO: skin paths
+		
+		Session.characters[char_res.name] = char_res
