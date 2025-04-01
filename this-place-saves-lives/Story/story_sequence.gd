129dbf8be26f40c0dc52ec4b7a1ef2e8052bd30c
Rai T <mira@trouvain.com>
2025-04-01 11:46:02 +0200

    added lists of characters in story events (really this time)

--- this-place-saves-lives/Story/story_sequence.gd
+++ this-place-saves-lives/Story/story_sequence.gd
@@ -11,3 +11,14 @@ static var event_order = [
 	"sam2",
 	"sarah2"
 ]
+
+static var event_characters = {
+	"honeyalyatutorial" : ["Honey", "Alya"],
+	"sam1" : ["Sam"],
+	"sarah1" : ["Sarah"],
+	"honeyalya1" : ["Honey", "Alya"],
+	"nickysam1" : ["Nicky", "Sam"],
+	"marel1" : ["Marel"],
+	"sam2" : ["Sam"],
+	"sarah2" : ["Sarah", "Honey"]
+}
