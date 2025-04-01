fc049f2ff1e00019a974956048fe83826f76ac22
Anieway <116640578+Anieway@users.noreply.github.com>
2025-04-01 19:40:16 +0200

    minor fixes for main game loop

--- this-place-saves-lives/Scripts/main.gd
+++ this-place-saves-lives/Scripts/main.gd
@@ -5,7 +5,6 @@ var character_base: PackedScene = preload("res://Characters/topdown_character.ts
 var spawn_timer: Timer
 #determines how many known characters without dialogue are spawned in between story events
 var non_story_spawn_count: int
-var character_pool: Array[String]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,13 +16,11 @@ func _ready() -> void:
 	%Room.check_capacities()
 	SignalBus.approval_delta.connect(update_approval)
 	#Initialize story at the beginning TODO do this dependant on save data
-	Session.next_event_index = 0
+	Session.next_event = 0
 	non_story_spawn_count = 0
-	character_pool = []
 	#set up a timer that spawns characters
 	spawn_timer = Timer.new()
-	spawn_timer.wait_time = \
-		randf_range(GlobalSettings.min_spawn_delay_s, GlobalSettings.max_spawn_delay_s)
+	spawn_timer.wait_time = 1.0
 	spawn_timer.autostart = true
 	spawn_timer.one_shot = false
 	spawn_timer.timeout.connect(
@@ -32,6 +29,9 @@ func _ready() -> void:
 				randf_range(GlobalSettings.min_spawn_delay_s, GlobalSettings.max_spawn_delay_s)
 			next_spawn()
 			)
+	add_child(spawn_timer)
+	SignalBus.dialog_start.connect(func():spawn_timer.paused = true)
+	SignalBus.dialog_end.connect(func():spawn_timer.paused = false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -65,20 +65,35 @@ func next_day():
 	get_tree().create_timer(1).timeout.connect(func(): %Day.visible = false)
 
 func next_spawn():
-	#TODO spawn non story characters in between
+	#spawn non story characters in between
+	var chara_name :String
+	var setting :CharacterSetting
+	assert(non_story_spawn_count >= 0, "non_story_spawn_count should never be less than 0!")
 	if non_story_spawn_count == 0:
-		spawn_character(StorySequence.event_order[Session.next_event_index])
-		Session.next_event_index += 1
+		chara_name = StorySequence.event_order[Session.next_event]
+		setting = load("res://Characters/" + chara_name + ".tres")
+		#TODO add characters once event is over
+		Session.known_characters.append_array(StorySequence.event_characters[chara_name])
+		Session.next_event += 1
+		non_story_spawn_count = \
+		randi_range(GlobalSettings.min_non_story_spawn, GlobalSettings.max_non_story_spawn)
 	else:
-		spawn_character(character_pool.pick_random())
+		chara_name = Session.known_characters.pick_random()
+		setting = Session.characters[chara_name]
 		non_story_spawn_count -= 1
-		
+	spawn_character(chara_name, setting)
 
-func spawn_character(name:String):
-	pass
+
+func spawn_character(chara_name:String, setting: CharacterSetting):
 	#test topdown character
 	#var test_character: topdown_character = character_base.instantiate()
-	#test_character.name = "Marel"
-	#test_character.character = load("res://Characters/Marel1.tres")
-	#test_character.position = %Room.spawn_point.position
-	#%Room.add_child(test_character)
+	var new_character:TopdownCharacter = character_base.instantiate()
+	#set node name to event name or generic character name
+	new_character.name = chara_name
+	#set event settings or generic settings
+	new_character.character = setting
+	#set character on spawn position
+	new_character.position = %Room.spawn_point.position
+	#add character to room (enables processing)
+	%Room.add_child(new_character)
+	
--- this-place-saves-lives/Scripts/topdown_character.gd
+++ this-place-saves-lives/Scripts/topdown_character.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-class_name topdown_character
+class_name TopdownCharacter
 
 const default_skin = preload("res://Characters/placeholder_material.tres")
 var room: Room
@@ -23,8 +23,6 @@ func _ready() -> void:
 		%Sprite.material = character.skin
 		#setup needs
 		current_needs = character.initial_needs.duplicate()
-		#set object name to character name
-		self.name = character.name
 	else: #TODO unnecessary in final game?
 		%Sprite.material = default_skin
 	
@@ -92,7 +90,7 @@ func enter_busy():
 	self.target_delta = Vector2.ZERO
 	match occupying_station.fulfills:
 		CharacterSetting.Need.talk:
-			SignalBus.dialog_waiting.emit(character.name)
+			SignalBus.dialog_waiting.emit(self.name)#use event name for dialog
 			#switch view
 			SignalBus.view_switch_desk.emit()
 			SignalBus.dialog_end.connect(
