3e1db546772baae392a3e9a5efdf817b8e3946d5
Rai T <mira@trouvain.com>
2025-04-01 13:18:22 +0200

    connect game state manager in main menu

--- this-place-saves-lives/main_menu.gd
+++ this-place-saves-lives/main_menu.gd
@@ -5,8 +5,9 @@ func _ready() -> void:
 
 
 func _on_play_texture_button_pressed() -> void:
-
-	get_tree().change_scene_to_file("res://Views/Main.tscn")
+	%GameStateManager.initialize_game()
+	%GameStateManager.start_game()
+	# get_tree().change_scene_to_file("res://Views/Main.tscn")
 
 
 func _on_settings_texture_button_pressed() -> void:
